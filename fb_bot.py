0be8e4e9169d021ac9463cb826469739fef45f92
Krendelev <krendelev@gmail.com>
2021-12-31 03:36:32 +0300

    add cart

--- fb_bot.py
+++ fb_bot.py
@@ -1,11 +1,13 @@
-import os
 import logging
+import os
+import re
 
 import requests
 from dotenv import load_dotenv
 from flask import Flask, request
 
 import fb_ui
+import store
 
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
@@ -27,29 +29,43 @@ def webhook():
     # print(data)
     try:
         messaging = data["entry"][0]["messaging"][0]
-        if text := messaging["message"].get("text"):
-            send_menu(messaging["sender"]["id"])
-            # send_message(messaging["sender"]["id"], text)
+        user_id = messaging["sender"]["id"]
+        if postback := messaging.get("postback"):
+            handle_user_request(user_id, postback.get("payload"))
+        elif messaging.get("message"):
+            menu = fb_ui.make_menu("main")
+            send_message(user_id, menu)
     except LookupError:
         return "Bad Request", 400
     else:
         return "OK", 200
 
 
-def send_menu(recipient_id):
+def handle_user_request(user_id, request):
+    pattern = re.compile(r"^[\da-f]{8}-(?:[\da-f]{4}-){3}[\da-f]{12}$")
+    # while request:
+    match request.split():
+        case ["MENU", page] if page in ["main", "special", "satisfying", "hot"]:
+            menu = fb_ui.make_menu(page)
+            send_message(user_id, menu)
+        case ["MENU", item]: # if item := pattern.fullmatch(request).group():
+            store.add_to_cart(user_id, item, 1)
+        case ["CART"]:
+            cart = fb_ui.make_cart(user_id)
+            send_message(user_id, cart)
+        case ["CART", item]: # if item := pattern.fullmatch(request).group():
+            store.remove_from_cart(user_id, item)
+            cart = fb_ui.make_cart(user_id)
+            send_message(user_id, cart)
+        case ["CHECKOUT"]:
+            ...
+
+
+def send_message(user_id, message):
     url = "https://graph.facebook.com/v2.6/me/messages"
     params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
     headers = {"Content-Type": "application/json"}
-    payload = {"recipient": {"id": recipient_id}, "message": fb_ui.make_menu("main")}
-    response = requests.post(url, params=params, headers=headers, json=payload)
-    response.raise_for_status()
-
-
-def send_message(recipient_id, message):
-    url = "https://graph.facebook.com/v2.6/me/messages"
-    params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
-    headers = {"Content-Type": "application/json"}
-    payload = {"recipient": {"id": recipient_id}, "message": {"text": message}}
+    payload = {"recipient": {"id": user_id}, "message": message}
     response = requests.post(url, params=params, headers=headers, json=payload)
     response.raise_for_status()
 
@@ -61,3 +77,5 @@ if __name__ == "__main__":
     )
     load_dotenv()
     app.run(debug=True)
+
+# "message": {"text": message}
--- fb_ui.py
+++ fb_ui.py
@@ -3,45 +3,99 @@ from dotenv import load_dotenv
 import fb_utils
 import store
 
+SHOP = {
+    "title": "PIZZERIA",
+    "image_url": "https://bit.ly/3z3gr4O",
+    "subtitle": "Лучшая пицца для вас!",
+    "buttons": [
+        {"type": "postback", "title": "Корзина", "payload": "CART"},
+        {"type": "postback", "title": "Оформить заказ", "payload": "CHECKOUT"},
+    ],
+}
+CATEGORIES = {
+    "title": "Не нашли пиццу по вкусу?",
+    "image_url": "https://bit.ly/33Wecoh",
+    "subtitle": "Посмотрите в разделах ниже",
+    "buttons": [
+        {"type": "postback", "title": "Особые", "payload": "MENU special"},
+        {"type": "postback", "title": "Сытные", "payload": "MENU satisfying"},
+        {"type": "postback", "title": "Острые", "payload": "MENU hot"},
+    ],
+}
+CART = {
+    "title": "Корзина",
+    "image_url": "https://bit.ly/3EHxxpY",
+    "buttons": [
+        {"type": "postback", "title": "Доставить", "payload": "deliver"},
+        {"type": "postback", "title": "Самовывоз", "payload": "pickup"},
+        {"type": "postback", "title": "Меню", "payload": "MENU main"},
+    ],
+}
+
 
 def make_menu(category):
-    elements = [
-        {
-            "title": f'{p["name"]} (₽{p["price"][0]["amount"]})',
-            "image_url": store.get_file_link(
-                p["relationships"]["main_image"]["data"]["id"]
-            ),
-            "subtitle": p["description"],
-            "buttons": [
-                {"type": "postback", "title": "Добавить в корзину", "payload": p["id"]},
-            ],
-        }
-        for p in fb_utils.get_products_by_category(category)
-    ]
-    elements.append(
-        {
-            "title": "Не нашли пиццу по вкусу?",
-            "image_url": "https://bit.ly/33Wecoh",
-            "subtitle": "Посмотрите в разделах ниже",
-            "buttons": [
-                {"type": "postback", "title": "Особые", "payload": "special"},
-                {"type": "postback", "title": "Сытные", "payload": "satisfying"},
-                {"type": "postback", "title": "Острые", "payload": "hot"},
-            ],
-        }
-    )
-    elements.append(
-        {
-            "title": "PIZZERIA",
-            "image_url": "https://bit.ly/3z3gr4O",
-            "subtitle": "Лучшая пицца для вас!",
-            "buttons": [
-                {"type": "postback", "title": "Корзина", "payload": "CART"},
-                {"type": "postback", "title": "Оформить заказ", "payload": "CHECKOUT"},
-            ],
-        }
-    )
+    elements = []
+    for p in fb_utils.get_products_by_category(category):
+        image_url = store.get_file_link(p["relationships"]["main_image"]["data"]["id"])
+        elements.append(
+            {
+                "title": f'{p["name"]} (₽{p["price"][0]["amount"]})',
+                "image_url": image_url,
+                "subtitle": p["description"],
+                "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Добавить в корзину",
+                        "payload": f"MENU {p['id']}",
+                    }
+                ],
+            }
+        )
 
+    elements.append(CATEGORIES)
+    elements.append(SHOP)
+
+    return {
+        "attachment": {
+            "type": "template",
+            "payload": {"template_type": "generic", "elements": elements},
+        }
+    }
+
+
+def make_cart(user_id):
+    cart = store.get_cart_items(user_id)
+    if not cart["data"]:
+        return {"text": "Ваша корзина пуста"}
+
+    elements = []
+    card = CART.copy()
+    total = cart["meta"]["display_price"]["with_tax"]["formatted"]
+    card["subtitle"] = f"Ваш заказ на сумму ₽{total}"
+    elements.append(card)
+    for p in cart["data"]:
+        price = p["meta"]["display_price"]["with_tax"]["unit"]["formatted"]
+        val = p["meta"]["display_price"]["with_tax"]["value"]["formatted"]
+        descr = f"Цена: ₽{price}\nВ корзине: {p['quantity']} на сумму ₽{val}"
+        elements.append(
+            {
+                "title": p["name"],
+                "image_url": p["image"]["href"],
+                "subtitle": descr,
+                "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Добавить ещё одну",
+                        "payload": f"MENU {p['product_id']}",
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Удалить из корзины",
+                        "payload": f"CART {p['id']}",
+                    },
+                ],
+            }
+        )
     return {
         "attachment": {
             "type": "template",
